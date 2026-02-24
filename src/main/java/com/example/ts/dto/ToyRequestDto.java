3acbd40484d2c0aeac91b028ed576eb6e51cad53
user <kabanovau271@gmail.com>
2026-02-24 22:20:07 +0300

    Fix import order

--- src/main/java/com/example/ts/dto/ToyRequestDto.java
+++ src/main/java/com/example/ts/dto/ToyRequestDto.java
@@ -1,41 +1,12 @@
 package com.example.ts.dto;
 
+import lombok.Data;
+
+@Data
 public class ToyRequestDto {
 
   private String name;
   private String brand;
   private Double price;
   private Integer quantity;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getBrand() {
-    return brand;
-  }
-
-  public void setBrand(String brand) {
-    this.brand = brand;
-  }
-
-  public Double getPrice() {
-    return price;
-  }
-
-  public void setPrice(Double price) {
-    this.price = price;
-  }
-
-  public Integer getQuantity() {
-    return quantity;
-  }
-
-  public void setQuantity(Integer quantity) {
-    this.quantity = quantity;
-  }
 }
--- src/main/java/com/example/ts/dto/ToyResponseDto.java
+++ src/main/java/com/example/ts/dto/ToyResponseDto.java
@@ -1,5 +1,8 @@
 package com.example.ts.dto;
 
+import lombok.Data;
+
+@Data
 public class ToyResponseDto {
 
   private Long id;
@@ -7,44 +10,4 @@ public class ToyResponseDto {
   private String brand;
   private Double price;
   private Integer quantity;
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getBrand() {
-    return brand;
-  }
-
-  public void setBrand(String brand) {
-    this.brand = brand;
-  }
-
-  public Double getPrice() {
-    return price;
-  }
-
-  public void setPrice(Double price) {
-    this.price = price;
-  }
-
-  public Integer getQuantity() {
-    return quantity;
-  }
-
-  public void setQuantity(Integer quantity) {
-    this.quantity = quantity;
-  }
 }
