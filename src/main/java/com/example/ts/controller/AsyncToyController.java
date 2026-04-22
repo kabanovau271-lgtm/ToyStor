a7f217ee94c0b07e4a0ebac82fa5177788fabc80
user <kabanovau271@gmail.com>
2026-04-22 17:33:52 +0300

    lab 6.1

--- src/main/java/com/example/ts/controller/AsyncToyController.java
+++ src/main/java/com/example/ts/controller/AsyncToyController.java
@@ -3,13 +3,18 @@ package com.example.ts.controller;
 import com.example.ts.async.AsyncToyService;
 import com.example.ts.dto.AsyncTaskResponse;
 import com.example.ts.dto.ToyRequestDto;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.Map;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Асинхронные операции с игрушками")
 @RestController
